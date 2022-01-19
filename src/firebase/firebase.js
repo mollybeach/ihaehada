3c019a19ceb9edf1f930cacc0292419d09d217f1
mollybeach <mollyjbeach@gmail.com>
2022-01-19 01:58:08 -0800

    successfully deploy

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,23 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAd-pX35BsCvIBDXstf_LmQaz82PNFGcMI",
-  authDomain: "fir-vue-startup-kit.firebaseapp.com",
-  databaseURL: "https://fir-vue-startup-kit.firebaseio.com",
-  projectId: "fir-vue-startup-kit",
-  storageBucket: "fir-vue-startup-kit.appspot.com",
-  messagingSenderId: "225404938985",
-  appId: "1:225404938985:web:a2cea9fab1722fce",
-  messageKey: "BGwIKr3yZdXmg4Pl-BpIQX_ETtHYl0RqvAJFoc-hYKFyaoU3wZXarqto5DSq1cemFuARMjFvJ02B83T5ga42MKo"
+  apiKey: "AIzaSyDkmekrpvVfoCBVsyrpDveOd1TlyzLGAu0",
+  authDomain: "ihaehada.firebaseapp.com",
+  databaseURL: "https://ihaehada-default-rtdb.firebaseio.com",
+  projectId: "ihaehada",
+  storageBucket: "ihaehada.appspot.com",
+  messagingSenderId: "422071066400",
+  appId: "1:422071066400:web:65df6d74b852aa91e24145",
+  measurementId: "G-JVF6XCS734"
 };
 
-export default firebaseConfig
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+export default firebaseConfig;
