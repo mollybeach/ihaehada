1dade8007c755ad032e03ac898c2fafb71859e8f
Molly Beach <mollyjbeach@gmail.com>
2022-04-05 12:20:48 -0700

    moved components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 
 import '@/plugins/vuetify'
-import App from '@/components/App'
+import App from '@/App'
 import router from '@/router'
 import store from '@/store/index.js';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
