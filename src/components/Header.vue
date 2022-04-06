807c8013ec76e40173d61add9a78ee4d11f1842b
Molly Beach <mollyjbeach@gmail.com>
2022-04-06 07:21:23 -0700

    added header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,137 +1,53 @@
 <template>
-<div>
-    <v-navigation-drawer fixed clipped app v-model="navBar">
-      <v-list dense class="pt-0">
-        <router-link to="/Signin" v-if="!logined">
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-icon>dashboard</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Signin</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </router-link>
-        <router-link to="/">
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-icon>dashboard</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Top</v-list-tile-title>  
-            </v-list-tile-content>
-          </v-list-tile>
-        </router-link>
-        <router-link to="/Message">
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-icon>dashboard</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Message</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </router-link>
-        <router-link to="/Protected" v-if="logined">
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-icon>dashboard</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Protected Page</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </router-link>
-        <router-link to="/Signout" v-if="logined">
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-icon>dashboard</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Signout</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </router-link>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar app >
-      <v-toolbar-side-icon @click.stop="updateNaviBar"></v-toolbar-side-icon>
-      <v-toolbar-title class="headline " >
-        <span>Korean Lessons</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat><router-link to="/hello">hello</router-link></v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-
-    <v-content>
-      <router-view v-if="loaded" />
-    </v-content>
-  </div>
+<div id="header"> 
+  <p class="logo">Logo</p>
+  <ul class="nav">
+       <!-- :key="headings.id">{{ sections | upcf }} </li>--> 
+  </ul>
+</div>
 </template>
 
 <script>
-import firebase from 'firebase/app' 
-import "firebase/auth"
-import "firebase/firestore"
-import "firebase/messaging"
-
-import firebaseConfig from '@/firebase/firebase';
-
-import store from '@/store/index.js';
-
 export default {
   name: 'Header',
-  data () {
-    return {
-      navBar: false,
-    }
+ data() {
+    return{
+    headings: [
+      //week1 : hello, to be, nice to meet you
+      { id: "home", values: [{  
+                                  header: "hello",
+                                  
+                              }] 
+            },
+      { id: "contact", values: [{  
+                                  header: "contact",
+                              }] 
+            },
+        { id: "about", values: [{  
+                                    header: "about",
+                                }] 
+                },
+          ]
+        }
+
   },
-  created() {
-    firebase.initializeApp(firebaseConfig);
-
-    // for messaging
-    if (firebase.messaging.isSupported() && firebaseConfig.messageKey) {
-      const messaging = firebase.messaging();
-      messaging.usePublicVapidKey(firebaseConfig.messageKey);
-
-      // Request Permission of Notifications
-      messaging.requestPermission().then(() => {
-        console.log('Notification permission granted.');
-        
-        // Get Token
-        messaging.getToken().then((token) => {
-          console.log(token)
-        })
-      }).catch((err) => {
-        console.log('Unable to get permission to notify.', err);
+  filters: {
+    upc: function(value){
+      return value.toUpperCase();
+    },
+    upcf: function(value){
+      value = value.split(' ');
+      value = value.map(function(value){
+        return value.charAt(0).toUpperCase() + value.slice(1);
       });
-    }
-    store.dispatch('setUser', { }) 
-  },
-  computed: {
-    logined() {
-      return !!this.$store.getters.getUser;
-    },
-    loaded() {
-      return !this.$store.getters.getUserLoading;
-    },
-  },
-  methods: {
-    updateNaviBar: function() {
-      this.navBar =  !this.navBar;
-    },
-  },
-}
-</script>
 
-<style>
-  p {
-  margin-bottom: 2px;
-  }
-  v-toolbar{
-    font-size: 1.5rem;
-    color: hotpink;
-  }
+      return value.join();
+    }
+    }
+}
+
+</script>
+<style >
+
+    
 </style>
